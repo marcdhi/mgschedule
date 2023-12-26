8eb2133ce5fbc5f3888fc1017ed4e672e77b36c2
marcdhi <gandhi.mardav@gmail.com>
2023-12-26 22:01:22 +0530

    app: add more feats

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from schema import Schedule
 from mgschedule_marcdhi import MGScheduler, MGJob
 import datetime
 import json
+import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -11,7 +16,51 @@ def main():
     return {"message": "Hello World"}
 
 def rule_based_api():
-    pass
+
+    with open('schedule.json', 'r') as file:
+        content = file.read()
+        if not content:
+            existing_data = []
+        else:
+            existing_data = json.loads(content)
+    
+    recurrence_weights = {"daily": 1, "weekly": 2, "monthly": 3}
+    sorted_jobs = sorted(existing_data, key=lambda x: (recurrence_weights.get(x["recur_at"], 0), x["time"]))
+    
+    print("Sorted jobs:", sorted_jobs)
+
+    # Check if today is equal to the next run date
+
+    today = datetime.datetime.now()
+    # string_today = today.strftime("%d-%m-%Y")
+    string_today = "25-01-2024"
+    # string_current_time = today.strftime("%I:%M %p")
+    string_current_time = "06:00 PM"
+
+    print("Today:", string_today)
+    print("Current time:", string_current_time)
+
+    for job in sorted_jobs:
+
+        next_run_day = job["next_run"].split(" ")[0]
+        next_run_time = job["next_run"].split(" ")[1] + " " + job["next_run"].split(" ")[2]
+
+        print("Next run day:", next_run_day)
+        print("Next run time:", next_run_time)
+
+        if string_today == next_run_day and string_current_time == next_run_time:
+            print("Today is the day!")
+            data_run = {
+                "model": job["model"],
+                "client": job["client"],
+                "bu": job["bu"],
+                "recur_at": job["recur_at"],
+                "time": job["time"]
+            }
+            response = requests.get("http://127.0.0.1:8000/", json=data_run)
+            print(response.text)
+        elif string_today != next_run_day:
+            print("Today is not the day!")
 
 @app.post("/schedule")
 def schedule(data: Schedule):
@@ -72,5 +121,7 @@ def schedule(data: Schedule):
 
     with open('schedule.json', 'w') as file:
         json.dump(existing_data, file, indent=2)
+
+    rule_based_api()
     
     return {"Your endpoint will next run on": next_run}
